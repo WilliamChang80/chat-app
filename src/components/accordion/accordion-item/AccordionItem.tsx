b8c623a5265b9ece21399ea2e0189e090fe500ee
WilliamChang80 <william_chang80@rocketmail.com>
2022-05-07 13:45:55 +0700

    Add temp code

--- src/components/accordion/accordion-item/AccordionItem.tsx
+++ src/components/accordion/accordion-item/AccordionItem.tsx
@@ -1,9 +1,19 @@
 export interface AccordionItemProps {
-    label: string
+  label: string;
+  iconLeft: JSX.Element | null;
+  iconRight: JSX.Element | null;
 }
 
-const AccordionItem = ({ label }: AccordionItemProps) => {
-    return (<div>{label}</div>)
-}
+const AccordionItem = ({ label, iconLeft, iconRight }: AccordionItemProps) => {
+  return (
+    <>
+      <span>
+        {iconLeft}
+        {label}
+        {iconRight}
+      </span>
+    </>
+  );
+};
 
 export default AccordionItem;
--- src/components/sidebar-server/SidebarServer.tsx
+++ src/components/sidebar-server/SidebarServer.tsx
@@ -1,32 +1,29 @@
-import Accordion from 'components/accordion'
-import AccordionItem from 'components/accordion/accordion-item'
-import Counter from 'redux/features/counter/Counter';
+import Accordion from "components/accordion";
+import AccordionItem from "components/accordion/accordion-item";
+import Counter from "redux/features/counter/Counter";
 
 const SidebarServer = () => {
-    const getItems = (): JSX.Element[] => {
-        return [
-            <AccordionItem label="asd" />,
-            <AccordionItem label="asd" />
-        ]
-    }
+  const getItems = (): JSX.Element[] => {
+    return [<AccordionItem label="asd" />, <AccordionItem label="asd" />];
+  };
 
-    return (
-        <aside className="w-1/4">
-            <div className="bg-primary h-screen flex flex-col divide-y-2 divide-black/25">
-                <ul className="space-y-2 py-3 px-3 bg-primary-dark hover:bg-primary-highlight">
-                    <li>
-                        <a href="#" className="text-white">
-                            <h1>Server Name</h1>
-                        </a>
-                    </li>
-                </ul>
-                <ul className="overflow-y-scroll ">
-                    <Accordion label="Channel A" items={getItems()} />
-                </ul>
-                <Counter />
-            </div>
-        </aside>
-    )
-}
+  return (
+    <aside className="w-1/4">
+      <div className="bg-primary h-screen flex flex-col divide-y-2 divide-black/25">
+        <ul className="space-y-2 py-3 px-3 bg-primary-dark hover:bg-primary-highlight">
+          <li>
+            <a href="#" className="text-white">
+              <h1>Server Name</h1>
+            </a>
+          </li>
+        </ul>
+        <ul className="overflow-y-scroll ">
+          <Accordion label="Channel A" items={getItems()} />
+        </ul>
+        <Counter />
+      </div>
+    </aside>
+  );
+};
 
-export default SidebarServer
+export default SidebarServer;
